b21ea52aeaf7b498bebeef1a13533f1a3a347ac3
gitPoisoN <brunovinicios2015@gmail.com>
2026-04-03 13:29:48 -0300

    Create SatsumaCarMechanicOverlay.lua

---
+++ media/lua/client/Vehicles/ISUI/SatsumaCarMechanicOverlay.lua
@@ -0,0 +1,25 @@
+ISCarMechanicsOverlay.CarList["Base.ZIL4"] = {imgPrefix = "satsuma_", x=0,y=0};
+
+ISCarMechanicsOverlay.PartList["Engine"] = {img="engine", vehicles={}};
+
+ISCarMechanicsOverlay.PartList["DoorFrontLeft"] = {img="door_front_left", vehicles={}};
+ISCarMechanicsOverlay.PartList["DoorFrontRight"] = {img="door_front_right", vehicles={}};
+
+ISCarMechanicsOverlay.PartList["HeadlightLeft"] = {img="headlight_left", vehicles={}};
+ISCarMechanicsOverlay.PartList["HeadlightRight"] = {img="headlight_right", vehicles={}};
+
+ISCarMechanicsOverlay.PartList["EngineDoor"] = {img="hood", vehicles={}};
+ISCarMechanicsOverlay.PartList["TruckBed"] = {img="trunk", vehicles={}};
+
+ISCarMechanicsOverlay.PartList["TireFrontLeft"] = {img="wheel_front_left", vehicles={}};
+ISCarMechanicsOverlay.PartList["TireFrontRight"] = {img="wheel_front_right", vehicles={}};
+ISCarMechanicsOverlay.PartList["TireRearLeft"] = {img="wheel_rear_left", vehicles={}};
+ISCarMechanicsOverlay.PartList["TireRearRight"] = {img="wheel_rear_right", vehicles={}};
+
+ISCarMechanicsOverlay.PartList["Windshield"] = {img="window_windshield", vehicles={}};
+ISCarMechanicsOverlay.PartList["WindshieldRear"] = {img="window_rear_windshield", vehicles={}};
+ISCarMechanicsOverlay.PartList["WindowFrontLeft"] = {img="window_front_left", vehicles={}};
+ISCarMechanicsOverlay.PartList["WindowFrontRight"] = {img="window_front_right", vehicles={}};
+ISCarMechanicsOverlay.PartList["WindowRearLeft"] = {img="window_rear_left", vehicles={}};
+ISCarMechanicsOverlay.PartList["WindowRearRight"] = {img="window_rear_right", vehicles={}};
+
