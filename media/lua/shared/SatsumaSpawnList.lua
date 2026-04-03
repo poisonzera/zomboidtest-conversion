7cfaa519831f0292f14f6f46f3db95ce99e70352
gitPoisoN <brunovinicios2015@gmail.com>
2026-04-03 13:31:13 -0300

    Create SatsumaSpawnList.lua

---
+++ media/lua/shared/SatsumaSpawnList.lua
@@ -0,0 +1,35 @@
+if VehicleZoneDistribution then
+
+
+VehicleZoneDistribution.trailerpark.vehicles["Base.Infernus"] = {index = -1, spawnChance = 5};
+
+
+VehicleZoneDistribution.bad.vehicles["Base.Infernus"] = {index = -1, spawnChance = 5};
+
+
+
+VehicleZoneDistribution.parkingstall.vehicles["Base.Infernus"] = {index = -1, spawnChance = 5};
+
+
+
+VehicleZoneDistribution.medium.vehicles["Base.Infernus"] = {index = -1, spawnChance = 5};
+
+
+
+VehicleZoneDistribution.good.vehicles["Base.Infernus"] = {index = -1, spawnChance = 5};
+
+
+VehicleZoneDistribution.sport.vehicles["Base.Infernus"] = {index = -1, spawnChance = 5};
+
+
+VehicleZoneDistribution.police.vehicles["Base.Infernus"] = {index = -1, spawnChance = 25};
+
+VehicleZoneDistribution.postal.vehicles["Base.Infernus"] = {index = -1, spawnChance = 25};
+
+
+VehicleZoneDistribution.ambulance.vehicles["Base.Infernus"] = {index = -1, spawnChance = 25};
+
+
+VehicleZoneDistribution.spiffo.vehicles["Base.Infernus"] = {index = -1, spawnChance = 35};
+
+end
